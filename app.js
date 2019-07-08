25034205b2b0b5f50d1e7c816f90f42ee6a24212
Fabio Chiarini <chiarrss@gmail.com>
2019-07-08 14:07:07 +0200

    Stylesheet now correctly loading

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const app = express();
 const routes = require("./routes/index");
 const books = require("./routes/books");
 
-//set view engine to use pug for template
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+//set view engine to use pug for template
 app.set("view engine", "pug");
 
 app.use("/", routes);
