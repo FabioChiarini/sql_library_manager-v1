c74eac715ec6f9924fcfdc8010670b0c2f929129
Fabio Chiarini <chiarrss@gmail.com>
2019-07-16 22:31:24 +0200

    Search bar working with pagination

--- routes/books.js
+++ routes/books.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
 var Book = require("../models").Book;
-const Op = require('sequelize').Op;
+const Op = require("sequelize").Op;
 
 let books_per_page = 5;
 
 router.get("/", (req, res) => {
   res.redirect("books/1");
 });
-
-
 
 /* "home" route, where al books are displayed */
 router.get("/:page", (req, res, next) => {
@@ -18,14 +16,14 @@ router.get("/:page", (req, res, next) => {
     raw: true,
     limit: books_per_page,
     order: [["createdAt", "DESC"]],
-    offset: (books_per_page * (req.params.page - 1))
+    offset: books_per_page * (req.params.page - 1)
   })
     .then(function(books) {
-      let number_of_pages = Math.ceil(books.count/books_per_page);
-      for(book in books.rows) {
+      let number_of_pages = Math.ceil(books.count / books_per_page);
+      for (book in books.rows) {
         books_to_show.push(books.rows[book]);
       }
-      res.render("index", { books: books_to_show , pages: number_of_pages});
+      res.render("index", { books: books_to_show, pages: number_of_pages });
     })
     .catch(err => {
       err.status = 500;
@@ -33,51 +31,53 @@ router.get("/:page", (req, res, next) => {
     });
 });
 
+router.get("/search/:searched_string/:page", (req, res, next) => {
+  let keyword = req.params.searched_string.toLowerCase();
 
-router.get("/search/:searched_string", (req, res, next) => {
-  let keyword = req.params.searched_string; 
-  console.log("AAQAAAA!    " + keyword);
-  //let books_to_show = [];
-  Book.findAll({
+  let books_to_show = [];
+  Book.findAndCountAll({
     raw: true,
     where: {
-
-      [Op.or] : [{
-        title: {
-          [Op.like]: `%${keyword}%`
+      [Op.or]: [
+        {
+          title: {
+            [Op.like]: `%${keyword}%`
+          }
+        },
+        {
+          author: {
+            [Op.like]: `%${keyword}%`
+          }
+        },
+        {
+          genre: {
+            [Op.like]: `%${keyword}%`
+          }
+        },
+        {
+          year: {
+            [Op.like]: `%${keyword}%`
+          }
         }
-      },
-      {
-        author: {
-          [Op.like]: `%${keyword}%`
-        }
-      },
-      {
-        genre: {
-          [Op.like]: `%${keyword}%`
-        }
-      },
-      {
-        year: {
-          [Op.like]: `%${keyword}%`
-        }
-      }
       ]
-
-    }
-    //limit: books_per_page,
-    //order: [["createdAt", "DESC"]]
-    //offset: (books_per_page * (req.params.page - 1))
+    },
+    limit: books_per_page,
+    order: [["createdAt", "DESC"]],
+    offset: books_per_page * (req.params.page - 1)
   })
     .then(function(books) {
-      res.render("search_results", {books: books});
-      /*
-      let number_of_pages = Math.ceil(books.count/books_per_page);
-      for(book in books.rows) {
+      let number_of_pages = Math.ceil(books.count / books_per_page);
+      for (book in books.rows) {
         books_to_show.push(books.rows[book]);
       }
-      res.render("index", { books: books_to_show , pages: number_of_pages});
-      */
+      console.log("books_to_show: " + books_to_show);
+      console.log("number_of_pages: " + number_of_pages);
+      console.log("keyword: " + keyword);
+      res.render("search_results", {
+        books: books_to_show,
+        pages: number_of_pages,
+        keyword: keyword
+      });
     })
     .catch(err => {
       err.status = 500;
@@ -85,15 +85,9 @@ router.get("/search/:searched_string", (req, res, next) => {
     });
 });
 
-
-
-
 router.post("/search", (req, res, next) => {
-  res.redirect("/books/search/" + req.body.keyword)
+  res.redirect("/books/search/" + req.body.keyword + "/1");
 });
-
-
-
 
 /* route that let the user insert other books and then displays the inserted book */
 router.get("/book/new", (req, res, next) => {
